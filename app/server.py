3bca0077d67fa1fdfc874cc2e53b7988fe8ff9de
Jerry Roback <robackjerry@gmail.com>
2020-02-05 14:30:50 -0600

    Update with ketchup identification model

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1H2Ahh1rm0n5OIEFeT1VyHDwv40rwjT0f'
+export_file_name = 'ketchup.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['heinz', 'hunts']
 path = Path(__file__).parent
 
 app = Starlette()
