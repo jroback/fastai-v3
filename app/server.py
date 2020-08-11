f9f1d405d6cb1162af7e56efaacf695071d403e6
Jerry Roback <robackjerry@gmail.com>
2020-08-11 14:50:13 -0500

    Update for vegetable classification

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1H2Ahh1rm0n5OIEFeT1VyHDwv40rwjT0f'
-export_file_name = 'ketchup.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1AsEQiAckS4hy5aVlBIhnwksBeIOqar78'
+export_file_name = 'export.pkl'
 
-classes = ['heinz', 'hunts']
+classes = ['broccoli', 'broccoli-rabe', 'brussels', 'cauliflower', 'kohlrabi']
 path = Path(__file__).parent
 
 app = Starlette()
